b7d6e7208478e772d7352dc0cd66b1d6c1e00291
Jeff <jlui820@gmail.com>
2020-07-24 14:36:40 -0700

    start guessing game

---
+++ game.js
@@ -0,0 +1,12 @@
+const secretNumber = 4;
+
+let stringGuess = prompt('Guess a number');
+let guess = Number(stringGuess);
+
+if (guess === secretNumber) {
+  alert('YOU GOT IT RIGHT!');
+} else if (guess > secretNumber) {
+  alert('Too high. Guess again!');
+} else {
+  alert('Too low. Guess again!');
+}
