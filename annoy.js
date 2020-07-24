a87d518470645ec079cc06b48fdc2329d876694a
Jeff <jlui820@gmail.com>
2020-07-24 15:12:14 -0700

    finish game to practice while loop

--- annoy.js
+++ annoy.js
@@ -6,11 +6,11 @@
 
 // alert('Yay, we made it!')
 
-// let answer = prompt('are we there yet?');
+let answer = prompt('are we there yet?');
 
-// while (answer.indexOf("yes") === -1) {
-//   let answer = prompt('are we there yet?');
-// }
+while (answer.indexOf("yes") === -1) {
+   answer = prompt('are we there yet?');
+}
 
-// alert('Yay, we made it!')
+alert('Yay, we made it!')
 
