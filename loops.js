ab941658414461d47cf5cb72366d2b5419765b03
Jeff <jlui820@gmail.com>
2020-07-24 14:57:55 -0700

    finish while loops practice

---
+++ loops.js
@@ -0,0 +1,33 @@
+// let counter = -10;
+
+// while (counter <= 19) {
+//   console.log(counter);
+//   counter++;
+// }
+
+// let counter = 10;
+
+// while (counter <= 40) {
+//   console.log(counter);
+//   counter += 2;
+// }
+
+// let counter = 300;
+
+// while (counter <= 333) {
+//   if (counter % 2 !== 0) {
+//     console.log(counter);
+//   }
+
+//   counter++;
+// }
+
+let counter = 5;
+
+while (counter <= 50) {
+  if (counter % 5 === 0 && counter % 3 === 0) {
+    console.log(counter);
+  }
+
+  counter++;
+}
