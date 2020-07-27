340ef7a8ec9994304f4d35b62eeaa08c4b9c69d9
Jeff <jlui820@gmail.com>
2020-07-27 12:25:35 -0700

    finish updated todos page

--- list.js
+++ list.js
@@ -3,18 +3,35 @@ let todos = ['buy new turtle', 'wash the dog'];
 let input = prompt('What would you like to do?');
 
 
-
-
 while (input !== 'quit') {
   if (input === 'list') {
-    console.log(todos);
+    listTodos();
   } else if (input === 'new') {
-    let newTodo = prompt('Enter new Todo');
-    todos.push(newTodo);
+    addTodo();
+  } else if (input === 'delete') {
+    deleteTodo();
   }
-
 
   input = prompt('What would you like to do?');
 }
 
-console.log('All chores finished!')
+console.log('All todos done!')
+
+function listTodos(){
+  console.log('**********')
+  todos.forEach(function (todo, i) {
+    console.log(i + ': ' + todo)
+  });
+  console.log('**********')
+}
+
+function addTodo(){
+  let newTodo = prompt('Enter new Todo');
+  todos.push(newTodo);
+  console.log('Added Todo');
+}
+
+function deleteTodo(){
+  let index = prompt('Enter index of todo to delete');
+  todos.splice(index, 1);
+}
