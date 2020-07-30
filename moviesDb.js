d7035b45838f1fbc14edec1d98b0bb5e1f49923f
Jeff <jlui820@gmail.com>
2020-07-29 20:56:06 -0700

    update moviesdb

--- moviesDb.js
+++ moviesDb.js
@@ -16,14 +16,49 @@ movies = [
   }
 ];
 
-let watchedMovies = (array) => {
-  for (let i = 0; i < array.length; i++) {
-    if(array[i].hasWatched === true) {
-      console.log ('You have watched ' + array[i].title + ' - ' + array[i].rating + 'stars')
-    } else {
-      console.log ('You have not seen ' + array[i].title + ' - ' + array[i].rating)
-    }
-  } 
+// let watchedMovies = (array) => {
+//   for (let i = 0; i < array.length; i++) {
+//     if(array[i].hasWatched) {
+//       console.log ('You have watched ' + array[i].title + ' - ' + array[i].rating + 'stars')
+//     } else {
+//       console.log ('You have not seen ' + array[i].title + ' - ' + array[i].rating)
+//     }
+//   } 
+// }
+
+"---------------------------------------"
+
+movies.forEach(movie => {
+  let result = 'You have ';
+  if (movie.hasWatched) {
+    result += 'watched ';
+  } else {
+    result += 'not seen ';
+  }
+
+  result += "\"" + movie.title + "\"";
+  result += " - " + movie.rating + ' stars';
+  console.log(result)
+})
+
+"--------------------------------------------"
+
+
+movies.forEach(function (movie) {
+  console.log(watchedMovies(movie))
+})
+
+function watchedMovies(movie) {
+  let result = 'You have ';
+  if (movie.hasWatched) {
+    result += 'watched ';
+  } else {
+    result += 'not seen ';
+  }
+
+  result += "\"" + movie.title + "\"";
+  result += " - " + movie.rating + ' stars';
+  return result
 }
 
-console.log(watchedMovies(movies));
+// console.log(watchedMovies(movies));
