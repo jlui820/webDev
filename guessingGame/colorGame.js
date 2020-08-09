8f1508af5a5bf455aceb1f9ba5c781329912be8c
Jeff <jlui820@gmail.com>
2020-08-09 16:34:58 -0700

    update styling for easy hard buttons

--- guessingGame/colorGame.js
+++ guessingGame/colorGame.js
@@ -1,20 +1,51 @@
-let colors = generateRandomColors(6);
-
+let numSquares = 6;
+let colors = generateRandomColors(numSquares);
 let squares = document.querySelectorAll('.square');
 let pickedColor = pickColor();
 let colorDisplay = document.getElementById('colorDisplay');
 let messageDisplay = document.querySelector('#message');
 let h1 = document.querySelector('h1');
 let resetButton = document.querySelector("#reset");
+let easyBtn = document.querySelector('#easyBtn');
+let hardBtn = document.querySelector('#hardBtn');
+
+easyBtn.addEventListener('click', function(){
+  hardBtn.classList.remove('selected');
+  easyBtn.classList.add('selected');
+  numSquares = 3;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for (let i = 0; i < squares.length; i++) {
+    if (colors[i]) {
+      squares[i].style.backgroundColor = colors[i];
+    } else {
+      squares[i].style.display = 'none';
+    }
+  }
+});
+
+hardBtn.addEventListener('click', function(){
+  hardBtn.classList.add('selected');
+  easyBtn.classList.remove('selected');
+  numSquares = 6;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+    squares[i].style.display = 'block';
+  }
+});
 
 resetButton.addEventListener('click', () => {
-  colors = generateRandomColors(6);
+  colors = generateRandomColors(numSquares);
   pickedColor = pickColor();
   colorDisplay.textContent = pickedColor;
   for (let i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = colors[i];
   }
-  h1.style.backgroundColor = '#232323';
+  h1.style.backgroundColor = '#steelblue';
 })
 
 colorDisplay.textContent = pickedColor;
@@ -45,7 +76,7 @@ let changeColors = color => {
 }
 
 function pickColor(){
-  let random = Math.floor(Math.random() * colors.length);
+  let random = Math.floor(Math.random() * colors.length); 
   return colors[random];
 }
 
